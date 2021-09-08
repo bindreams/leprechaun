7fdb775256828c182480dfaf2a5c7e098cf7aa7c
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2021-09-08 14:38:34 +0300

    Make data dir crossplatform

--- leprechaun/__init__.py
+++ leprechaun/__init__.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+from appdirs import user_data_dir
 
 dir = Path(__file__).parent
 sdata_dir = dir / "data"  # Static data dir
-data_dir = Path.home() / "AppData" / "Local" / "leprechaun"
+data_dir = Path(user_data_dir("leprechaun", appauthor=False, roaming=False))
 miners_dir = data_dir / "miners"
 miner_crashes_dir = data_dir / "miner-crashes"
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup, find_packages
 
 install_requires = [
+    "appdirs",
     "cachetools",
     "pywin32; platform_system=='Windows'",
     "pyyaml",
