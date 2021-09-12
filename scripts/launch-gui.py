25076c1fed8bc617b9c503086e306b4709cdcc1e
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2021-09-12 17:00:26 +0300

    Add CREATE_NO_WINDOW to gui shim

--- scripts/launch-gui.py
+++ scripts/launch-gui.py
@@ -1,7 +1,12 @@
 """Shim that runs Leprechaun GUI. Used during freezing in freeze-gui.spec."""
 import sys
 from pathlib import Path
-from subprocess import Popen
+import subprocess as sp
+
+if sys.platform == "win32":
+    flags = sp.CREATE_NO_WINDOW
+else:
+    flags = 0
 
 dir = Path(sys.argv[0]).parent
-Popen([dir / "leprechaun.exe", "-gp"])
+sp.Popen([dir / "leprechaun.exe", "-gp"], creationflags=flags)
